1a60b4e3c254b01855d65ceb4274ce4cbfb55ff7
lerd robott <lerdrobott@gmail.com>
2022-05-25 16:14:01 +0800

    initialization

---
+++ backend/pointwestpowerhouse/src/main/java/pointwestproleague/pointwestpowerhouse/PointwestpowerhouseApplication.java
@@ -0,0 +1,13 @@
+package pointwestproleague.pointwestpowerhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointwestpowerhouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointwestpowerhouseApplication.class, args);
+	}
+
+}
---
+++ backend/pointwestpowerhouse/src/test/java/pointwestproleague/pointwestpowerhouse/PointwestpowerhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package pointwestproleague.pointwestpowerhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointwestpowerhouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
